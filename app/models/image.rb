184f6131b9889fe071cee3e5aa89e30d52cb7b4d
David Clark Moore <davidclarkmoore@gmail.com>
2015-08-31 19:39:59 -0400

    update images to new small size for tiles

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
 	belongs_to :post
 
-	has_attached_file :photo, :styles => { :large => "1500x1500>", :medium => "800x800>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :photo, :styles => { :large => "1500x1500>", :medium => "800x800>", :small => "400x400", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
